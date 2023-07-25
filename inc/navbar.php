f47284b00cdb55984f17cb2ae86b7e52c6aeb026
egmprog <egmprog@gmail.com>
2023-07-25 01:43:39 -0300

    20230724 inicio

---
+++ inc/navbar.php
@@ -0,0 +1,68 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+
+    <div class="navbar-brand">
+        <a class="navbar-item" href="index.php?vista=home">
+        <img src="./img/logo.png" width="100" height="150" >
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+        
+        <?php if($_SESSION['rol']=="Editor"){ ?>
+
+        <div class="navbar-start">
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">Usuarios</a>
+
+                <div class="navbar-dropdown">
+                    <a href="index.php?vista=user_new" class="navbar-item">Nuevo</a>
+                    <a href="index.php?vista=user_list" class="navbar-item">Lista</a>
+                    <a href="index.php?vista=user_search" class="navbar-item">Buscar</a>
+                </div>
+            </div>
+
+            
+
+        </div>
+
+        <?php }else{  ?>
+
+        <div class="navbar-start">
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">Usuarios</a>
+
+                <div class="navbar-dropdown">
+                    
+                    <a href="index.php?vista=user_list" class="navbar-item">Lista</a>
+                    <a href="index.php?vista=user_search" class="navbar-item">Buscar</a>
+                </div>
+            </div>
+
+            
+
+        </div>
+        <?php }  ?>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a href="index.php?vista=user_update&user_id_up=<?php echo $_SESSION['id']; ?>" class="button is-primary is-rounded">
+                        Mi cuenta
+                    </a>
+
+                    <a href="index.php?vista=logout" class="button is-link is-rounded">
+                        Salir
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
